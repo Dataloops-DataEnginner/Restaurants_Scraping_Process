271e93359d9cd64e8da748cdb6bea662a764579f
Dataloops-DataEnginner <dataloopsdataengineerkw@gmail.com>
2025-05-11 16:24:30 +0300

    Update drive.py

--- drive.py
+++ drive.py
@@ -6,7 +6,7 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from retry import retry
 
-class SavingOnDrive:
+class SaveOnDrive:
     """Class to handle uploading files to Google Drive with date-based folders"""
     
     def __init__(self, credentials_json=None):
@@ -185,7 +185,7 @@ class SavingOnDrive:
 # from googleapiclient.http import MediaFileUpload
 
 
-# class SavingOnDrive:
+# class SaveOnDrive:
 #     """Class to handle uploading files to Google Drive with date-based folders"""
     
 #     def __init__(self, credentials_path='credentials.json'):
